923dd9d41ee8630071c64354a346648ce66b86f0
Asiimire mark <asiimiremark@gmail.com>
2018-04-20 16:51:01 +0300

    mapping

---
+++ app/src/main/java/com/example/asiimiremark/cityguide/MapsActivity.java
@@ -0,0 +1,171 @@
+package com.example.asiimiremark.cityguide;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnMarkerClickListener,LocationListener{
+
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+
+        }
+        //check the network is enabled
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+
+        locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                //instantiate the class,latLng
+                LatLng latLng=new LatLng(latitude,longitude);
+                // instantiate the class, Geocoder
+                Geocoder geocoder=new Geocoder(getApplicationContext());
+                try {
+                    List<Address>addressList= geocoder.getFromLocation(latitude,longitude,1);
+                    String str=addressList.get(0).getLocality()+",";
+                    str +=addressList.get(0).getCountryName();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
+    }
+else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                public MarkerOptions options;
+
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    //instantiate the class,latLng
+                    LatLng latLng=new LatLng(latitude,longitude);
+                    // instantiate the class, Geocoder
+                    Geocoder geocoder=new Geocoder(getApplicationContext());
+                    try {
+                        List<Address>addressList= geocoder.getFromLocation(latitude,longitude,1);
+                        String str=addressList.get(0).getLocality()+",";
+                        str +=addressList.get(0).getCountryName();
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                         mMap.addMarker(options);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }
+}
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        goToLocation(1.3733,32.2903);
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+      //  mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+      //  mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10.2f));
+    }
+
+    private void goToLocation(double lat, double lng) {
+        LatLng ll=new LatLng(lat,lng);
+        CameraUpdate update=CameraUpdateFactory.newLatLng(ll);
+        mMap.moveCamera(update);
+    }
+
+
+    private void goToLocationZoom(double lat, double lng,float zoom) {
+        LatLng ll=new LatLng(lat,lng);
+        CameraUpdate update=CameraUpdateFactory.newLatLngZoom(ll,zoom);
+        mMap.moveCamera(update);
+    }
+}
